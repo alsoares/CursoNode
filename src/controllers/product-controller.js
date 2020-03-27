e2c93da5e7a66cb4595de470936cb6775a6ec167
andre.soares <andre.soares@zup.com.br>
2020-03-27 13:52:13 -0300

    add getById

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,27 +1,59 @@
 'use strict'
 
 const mongoose = require('mongoose');
-const Product = mongoose.model('Product'); 
+const Product = mongoose.model('Product');
+
+exports.get = (req, res, next) => {
+    Product
+        .find({active: true}, 'title price slug')
+        .then(x => {
+            res
+                .status(200)
+                .send(x);
+        })
+        .catch(e => {
+            res
+                .status(400)
+                .send(e);
+        });
+
+};
+
+exports.getById = (req, res, next) => {
+    Product
+        .findById(req.params.id, 'title price slug')
+        .then(x => {
+            res
+                .status(200)
+                .send(x);
+        })
+        .catch(e => {
+            res
+                .status(400)
+                .send(e);
+        });
+
+}
 
 exports.post = (req, res, next) => {
     var product = new Product(req.body);
     product.save()
-    .then(x => {
-        res
-        .status(201)
-        .send({
-            message: 'Produto cadastrado com sucesso!'
-        });
-    }).catch(x=> {
+        .then(x => {
+            res
+                .status(201)
+                .send({
+                    message: 'Produto cadastrado com sucesso!'
+                });
+        }).catch(x => {
 
-        res
-        .status(400)
-        .send({
-            message: 'Falha ao cadastrar produto',
-            data: x
+            res
+                .status(400)
+                .send({
+                    message: 'Falha ao cadastrar produto',
+                    data: x
+                });
         });
-    });
-    
+
 };
 
 exports.put = (req, res, next) => {
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 const controller = require('../controllers/product-controller.js');
 
+router.get('/', controller.get);
+router.get('/:id', controller.getById);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/:id', controller.delete);
